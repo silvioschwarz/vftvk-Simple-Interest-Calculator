390a36b2186392857dc9b24f8396a99204b9367d
Silvio Schwarz <admin@silvioschwarz.com>
2021-01-18 13:20:57 +0100

    finished the assignement

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //Get input values
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    //Check whether principal is > 0 and focus on pricipal
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+
+    //Calculate interest and year in futer
+    var interest = principal * years * rate / 100;
+    var calculatedYear = new Date().getFullYear()+ parseInt(years);
+
+    var result = document.getElementById("result");
+
+    // Set result output
+    result.innerHTML = "If you deposit <mark>" + principal.toString()+ 
+    "</mark>,<br>at an interest rate of <mark>" + rate.toString() +
+    "</mark>%.<br>You will receive an amount of <mark>" + interest.toString() +
+    "</mark>,<br>in the year <mark>" + calculatedYear.toString()+"</mark>";
+
+
+
 }
-        
+
+function updateRateSpan()
+{
+    // Get handles
+    var rangeSliderValue = document.getElementById("rate").value;
+    var rateSpan = document.getElementById("rateSpan");
+
+    //Update rate span text
+    rateSpan.innerHTML = rangeSliderValue.toString() + " %";
+}
